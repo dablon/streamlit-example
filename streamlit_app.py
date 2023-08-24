82baa8890ddf3ead3b3bb54584b291abee078e1d
nicolas <nicolasalca@hotmail.com>
2023-08-23 22:50:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,45 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
+from langchain import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (ChatPromptTemplate,
+                                    HumanMessagePromptTemplate,
+                                    SystemMessagePromptTemplate)
+from langchain.document_loaders import *
+from langchain.chains.summarize import load_summarize_chain
+import tempfile
+from langchain.docstore.document import Document
 
-"""
-# Welcome to Streamlit!
+st.title("MapsGeneratorChat")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+# Initialize state variables
+chatbot_response = ""
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Generate a chatbot response for the given instruction
+def chatbotResponse():
+    chat = ChatOpenAI(
+        model="gpt-3.5-turbo-16k",
+        temperature=0.7
+    )
+    system_template = """You are a chatbot designed to generate responses based on given instructions."""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+    human_template = """Please generate a response for the given instruction."""
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
+    result = chain.run({})
+    return result # returns string   
 
+# Display the generated chatbot response to the user
+def display_response(chatbot_response):
+    if chatbot_response != "":
+        st.markdown(chatbot_response)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+# Get input from the user
+if st.button("Generate Chatbot Response"):
+    chatbot_response = chatbotResponse()
 
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Call functions only if all user inputs are taken and the button is clicked.
+display_response(chatbot_response)
